fe4818b0ad05abbbe6d088c386b379f4d0e2a444
Cyril Cartoux <6ssou@protonmail.com>
2020-06-08 14:58:57 +0200

    add invoices and user can request them' q q

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,7 @@
 const Product = require('../models/product');
 const Order = require("./../models/order");
+const fs = require("fs")
+const path = require("path")
 
 // /products page get all products
 exports.getProducts = (req, res, next) => {
@@ -145,3 +147,17 @@ exports.getOrders = (req, res, next) => {
       return next(error)
     })
 };
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  const invoiceName = 'invoice-' + orderId + '.pdf';
+  const invoicePath = path.join('data', 'invoices', invoiceName)
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+    res.setHeader("Content-Type", 'application/pdf')
+    res.setHeader("Content-Disposition", 'inline; filename="' + invoiceName + '"');
+    res.send(data);
+  })
+}
--- routes/shop.js
+++ routes/shop.js
@@ -15,6 +15,8 @@ router.get('/products/:productId', shopController.getProduct);
 router.get('/cart', isAuth, shopController.getCart);
 router.get('/orders', isAuth, shopController.getOrders);
 
+router.get('/orders/:orderId', isAuth, shopController.getInvoice);
+
 router.post('/cart', isAuth, shopController.postCart);
 router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
 router.post('/create-order', isAuth, shopController.postOrder);
