4cc76ee838eaa513ac3195f4322e1c83e474891b
Cyril Cartoux <6ssou@protonmail.com>
2020-06-09 14:06:05 +0200

    add stripe paiements

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,6 +3,10 @@ const Order = require("./../models/order");
 const fs = require("fs")
 const path = require("path")
 const PDFDocument = require("pdfkit")
+// Stripe : 
+const privateKey = require("./../util/database").privateKey
+const stripe = require("stripe")(privateKey)
+
 // objects stored by page 
 const ITEMS_PER_PAGE = 2;
 
@@ -135,6 +139,84 @@ exports.postCartDeleteProduct = (req, res, next) => {
       return next(error)
     });
 };
+
+exports.getCheckout = (req, res, next) => {
+  let products
+  let total = 0;
+  req.user
+    .populate('cart.items.productId')
+    // to return a promise 
+    .execPopulate()
+    .then(user => {
+      products = user.cart.items
+      total = 0;
+      products.forEach(p => {
+        total += p.quantity * p.productId.price;
+      })
+
+      return stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: products.map(p => {
+          return {
+            name: p.productId.title,
+            description: p.productId.description,
+            // in cents
+            amount: p.productId.price * 100,
+            currency: 'usd',
+            quantity: p.quantity
+          };
+        }),
+        success_url: req.protocol + '://' + req.get("host") + "/checkout/success",
+        cancel_url:  req.protocol + '://' + req.get("host") + "/checkout/cancel"
+      });
+
+      
+    })
+    .then(session => {
+      res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout',
+        products: products,
+        totalSum: total,
+        sessionId: session.id
+      });
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error)
+    });
+}
+
+exports.getCheckoutSuccess = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    // to return a promise 
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } }
+      })
+      const order = new Order({
+        user: {
+          email: req.user.email,
+          userId: req.user
+        },
+        products: products
+      })
+      return order.save();
+    })
+    .then(() => {
+      return req.user.clearCart();
+    }).then(() => {
+      res.redirect("/orders")
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error)
+    })
+}
 
 // create orders
 exports.postOrder = (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -14,11 +14,12 @@ router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
 router.get('/cart', isAuth, shopController.getCart);
 router.get('/orders', isAuth, shopController.getOrders);
-
 router.get('/orders/:orderId', isAuth, shopController.getInvoice);
+router.get('/checkout', isAuth, shopController.getCheckout);
+router.get('/checkout/success', isAuth, shopController.getCheckoutSuccess);
+router.get('/checkout/cancel', isAuth, shopController.getCheckout);
 
 router.post('/cart', isAuth, shopController.postCart);
 router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
-router.post('/create-order', isAuth, shopController.postOrder);
 
 module.exports = router;
